1b3ff45e0fe1f3ca6e549060c74fc65dbffb6b84
sanarajput123 <141543799+sanarajput123@users.noreply.github.com>
2023-10-23 21:05:11 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+// Function to append text to the display
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+// Function to clear the display
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+// Function to calculate the result
+function calculateResult() {
+    try {
+        const result = eval(document.getElementById('display').value);
+        document.getElementById('display').value = result;
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
+
